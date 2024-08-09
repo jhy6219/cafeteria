1adceac51ddab073e8e98a2b732b4b02ebe52184
haeyoon <jhy6219@naver.com>
2024-08-09 17:50:49 +0900

    update

--- setup/default.py
+++ setup/default.py
@@ -5,5 +5,10 @@ import pandas as pd
 import ast
 from fuzzywuzzy import fuzz
 from fuzzywuzzy import process
+
+import datetime
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
 pd.set_option('display.max_columns', None)
 tqdm.pandas()
